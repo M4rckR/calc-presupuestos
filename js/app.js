d97b4517af15c06771644af5b96563f7b3059b1d
Marcos Romero <-e>
2023-12-11 10:13:44 -0500

    Pagina terminada, todo funcional  ('Optimizado')

--- js/app.js
+++ js/app.js
@@ -1,34 +1,49 @@
-const btnPresupuesto =  document.querySelector('#ingreso-presupuesto');
+// Seleccionar elementos del DOM
+const btnPresupuesto = document.querySelector('#ingreso-presupuesto');
 const campoPresupuesto = document.querySelector('#campo-presupuesto');
-const formulario1 =  document.querySelector('#formulario1');
-const agregarGasto =  document.querySelector('#agregar-gasto');
-const campoNombre =  document.querySelector('#campo-nombre');
-const campoGasto =  document.querySelector('#campo-gasto');
-const reiniciar =  document.querySelector('#reiniciar');
-const listaGastos =  document.querySelector('#listado-gastos');
+const formulario1 = document.querySelector('#formulario1');
+const agregarGasto = document.querySelector('#agregar-gasto');
+const campoNombre = document.querySelector('#campo-nombre');
+const campoGasto = document.querySelector('#campo-gasto');
+const reiniciar = document.querySelector('#reiniciar');
+const listaGastos = document.querySelector('#listado-gastos');
 const formulario2 = document.querySelector('#formulario2');
-let itemsGasto = []
 
+// Arreglo para almacenar los gastos
+let itemsGasto = [];
 
+// Agregar listeners a los elementos
 eventListeners();
+
 function eventListeners() {
-    // Funcion por defecto
-    document.addEventListener('DOMContentLoaded', funcionesPorDefecto)
-    btnPresupuesto.addEventListener('click', validarPresupuesto)
-    agregarGasto.addEventListener('click', validarGasto)
+    // Cargar funciones por defecto al cargar el DOM
+    document.addEventListener('DOMContentLoaded', funcionesPorDefecto);
+
+    // Validar presupuesto al hacer clic en el botón
+    btnPresupuesto.addEventListener('click', validarPresupuesto);
+
+    // Validar gasto al hacer clic en el botón
+    agregarGasto.addEventListener('click', validarGasto);
+
+    // Reiniciar la aplicación al hacer clic en el botón
     reiniciar.addEventListener('click', () => {
+        // Habilitar campos y botones
         ui.gastosToggle(true);
         ui.presupuestoToggle(false);
+
+        // Limpiar el HTML
         ui.limpiarHtml();
+
+        // Reiniciar formulario
         formulario1.reset();
+
+        // Reiniciar variables
         presupuesto = undefined;
         itemsGasto = [];
-    })
-
+    });
 }
 
-// Clases
-
+// Clase Presupuesto
 class Presupuesto {
     constructor(presupuesto) {
         this.presupuesto = presupuesto;
@@ -38,163 +53,168 @@ class Presupuesto {
 
     actualizarRestante(gasto) {
         this.restante -= gasto;
-        this.gastos = [...this.gastos, gasto]
+        this.gastos = [...this.gastos, gasto];
     }
-
 }
 
+// Clase UI
 class Ui {
+    // Deshabilitar/activar elementos relacionados con los gastos
     gastosToggle(bandera) {
-        const elements = [campoNombre, campoGasto, agregarGasto, reiniciar]
+        const elements = [campoNombre, campoGasto, agregarGasto, reiniciar];
         elements.forEach(element => {
             element.disabled = bandera;
-            element.classList.toggle('cursor-not-allowed', bandera)
-            element.classList.toggle('opacity-50',bandera)
+            element.classList.toggle('cursor-not-allowed', bandera);
+            element.classList.toggle('opacity-50', bandera);
         });
     }
 
+    // Deshabilitar/activar elementos relacionados con el presupuesto
     presupuestoToggle(bandera) {
-        const elements = [campoPresupuesto, btnPresupuesto]
+        const elements = [campoPresupuesto, btnPresupuesto];
         elements.forEach(element => {
             element.disabled = bandera;
-            element.classList.toggle('cursor-not-allowed', bandera)
-            element.classList.toggle('opacity-50',bandera)
+            element.classList.toggle('cursor-not-allowed', bandera);
+            element.classList.toggle('opacity-50', bandera);
         });
     }
 
-    generarAlerta(mensaje,tipo) {
+    // Generar una alerta en el formulario
+    generarAlerta(mensaje, tipo) {
         formulario1.reset();
         const alerta = document.createElement('div');
         alerta.id = 'alerta';
         alerta.textContent = mensaje;
-        alerta.className = 'py-2 px-4 text-white mt-2 text-center'
+        alerta.className = 'py-2 px-4 text-white mt-2 text-center';
 
-        if(tipo === 'error') {
-            alerta.classList.add('bg-red-500')
-        }
-        else {
-            alerta.classList.add('bg-green-600')
-        }
-
-        if(!formulario1.querySelector('#alerta')){
-            formulario1.appendChild(alerta)
+        if (tipo === 'error') {
+            alerta.classList.add('bg-red-500');
+        } else {
+            alerta.classList.add('bg-green-600');
         }
 
+        if (!formulario1.querySelector('#alerta')) {
+            formulario1.appendChild(alerta);
+        }
 
         setTimeout(() => {
             alerta.remove();
         }, 2000);
     }
 
-    establecerPresupuesto(restante){
-        const labelPresupuesto = document.querySelector('#presupuesto')
-        const labelRestante = document.querySelector('#restante')
+    // Establecer el presupuesto y el restante en el HTML
+    establecerPresupuesto(restante) {
+        const labelPresupuesto = document.querySelector('#presupuesto');
+        const labelRestante = document.querySelector('#restante');
         labelPresupuesto.textContent = restante;
         labelRestante.textContent = restante;
     }
 
-    ingresarGasto(){
+    // Mostrar los gastos en el HTML
+    ingresarGasto() {
         this.limpiarHtml();
         itemsGasto.forEach(gasto => {
-            const {nombre,valor,id} = gasto;
+            const { nombre, valor, id } = gasto;
             const itemGasto = document.createElement('div');
             const btnCerrar = document.createElement('button');
 
-            btnCerrar.textContent = 'X'
-            btnCerrar.className = 'bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 ml-auto' 
-            btnCerrar.setAttribute('data-id',id)
+            btnCerrar.textContent = 'X';
+            btnCerrar.className =
+                'bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 ml-auto';
+            btnCerrar.setAttribute('data-id', id);
             btnCerrar.onclick = () => {
-                this.eliminarGasto(btnCerrar.getAttribute('data-id'))
-                presupuesto.actualizarRestante(-valor)
-                this.actualizarRestante(presupuesto.restante)
-            }
-            itemGasto.className = 'flex items-center bg-white py-2 px-4 mb-2'
-            itemGasto.textContent = `${nombre} - $${valor}`
+                this.eliminarGasto(btnCerrar.getAttribute('data-id'));
+                presupuesto.actualizarRestante(-valor);
+                this.actualizarRestante(presupuesto.restante);
+            };
+            itemGasto.className = 'flex items-center bg-white py-2 px-4 mb-2';
+            itemGasto.textContent = `${nombre} - $${valor}`;
 
-            itemGasto.appendChild(btnCerrar)
-            listaGastos.appendChild(itemGasto)
+            itemGasto.appendChild(btnCerrar);
+            listaGastos.appendChild(itemGasto);
             formulario2.reset();
-            // console.log(itemsGasto);
-        })
-        
+        });
     }
 
-    limpiarHtml(){
-        while(listaGastos.firstChild){
-            listaGastos.removeChild(listaGastos.firstChild)
+    // Limpiar el HTML de los gastos
+    limpiarHtml() {
+        while (listaGastos.firstChild) {
+            listaGastos.removeChild(listaGastos.firstChild);
         }
     }
 
+    // Actualizar el restante en el HTML
     actualizarRestante(restante) {
-        const labelRestante = document.querySelector('#restante')
+        const labelRestante = document.querySelector('#restante');
         labelRestante.textContent = restante;
     }
 
+    // Eliminar un gasto del arreglo y actualizar el HTML
     eliminarGasto(id) {
-        //Funcion para eliminar un gasto en base al id como parametro
-        itemsGasto = itemsGasto.filter(gasto => gasto.id != id)
+        itemsGasto = itemsGasto.filter(gasto => gasto.id != id);
         this.ingresarGasto();
     }
 }
+
+// Instancia de la clase Ui
 const ui = new Ui();
+
+// Variable para almacenar el presupuesto
 let presupuesto;
 
 // Funciones
 
+// Funciones por defecto al cargar el DOM
 function funcionesPorDefecto() {
     ui.gastosToggle(true);
 }
 
-function validarPresupuesto(e){
+// Validar el presupuesto ingresado
+function validarPresupuesto(e) {
     e.preventDefault();
     const valorIngresado = e.target.previousElementSibling.value;
-    
-    if(valorIngresado === '' ||  isNaN(valorIngresado)){
-        ui.generarAlerta('Ingrese un presupuesto valido','error');
+
+    if (valorIngresado === '' || isNaN(valorIngresado)) {
+        ui.generarAlerta('Ingrese un presupuesto válido', 'error');
         return;
-    }
-    else if(valorIngresado <= 0){
-        ui.generarAlerta('El presupuesto no puede ser 0 o menor', 'error')
+    } else if (valorIngresado <= 0) {
+        ui.generarAlerta('El presupuesto no puede ser 0 o menor', 'error');
         return;
-    }
-    else {
-        ui.generarAlerta('El valor ha sido ingresado')
+    } else {
+        ui.generarAlerta('El valor ha sido ingresado');
         ui.establecerPresupuesto(valorIngresado);
         ui.gastosToggle(false);
         ui.presupuestoToggle(true);
     }
 
     presupuesto = new Presupuesto(valorIngresado);
-    let {restante} = presupuesto
+    let { restante } = presupuesto;
     ui.establecerPresupuesto(restante);
 }
 
-function validarGasto(e){
+// Validar el gasto ingresado
+function validarGasto(e) {
     const valorNombre = campoNombre.value;
     const valorGasto = campoGasto.value;
 
-
-    if(valorNombre === '' || valorGasto === ''){
-        ui.generarAlerta('Ingrese un nombre y un gasto','error')
+    if (valorNombre === '' || valorGasto === '') {
+        ui.generarAlerta('Ingrese un nombre y un gasto', 'error');
         return;
     }
-    if(valorGasto <= 0 || isNaN(valorGasto)){
-        ui.generarAlerta('Ingrese un gasto valido','error')
+    if (valorGasto <= 0 || isNaN(valorGasto)) {
+        ui.generarAlerta('Ingrese un gasto válido', 'error');
         return;
-    }
-    else {
+    } else {
         const gasto = {
             nombre: valorNombre,
             valor: valorGasto,
             id: Date.now()
-        }
-        itemsGasto.push(gasto)
-        presupuesto.actualizarRestante(valorGasto)
-        let {gastos,restante} = presupuesto;
-        ui.actualizarRestante(restante)
-        ui.generarAlerta('El gasto ha sido ingresado')
-        ui.ingresarGasto()
-
+        };
+        itemsGasto.push(gasto);
+        presupuesto.actualizarRestante(valorGasto);
+        let { gastos, restante } = presupuesto;
+        ui.actualizarRestante(restante);
+        ui.generarAlerta('El gasto ha sido ingresado');
+        ui.ingresarGasto();
     }
 }
-
